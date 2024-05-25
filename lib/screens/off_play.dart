d2a29ab0e296333184b04a479d2c5fbfe7dc85d2
Pranjal Chaplot <pranjalchaplot@gmail.com>
2024-05-25 16:01:07 +0530

    replacing offlineplay with offplay

--- lib/screens/off_play.dart
+++ lib/screens/off_play.dart
@@ -1,12 +1,13 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:imposter_syndrome_game/models/game_card.dart';
-import 'package:imposter_syndrome_game/services/game_logic.dart';
+import 'package:imposter_syndrome_game/models/enums/game_stage_enum.dart';
+import 'package:imposter_syndrome_game/services/dialogs.dart';
+import 'package:imposter_syndrome_game/widgets/display_game_cards.dart';
+import 'package:imposter_syndrome_game/widgets/display_selection_cards.dart';
+import 'package:provider/provider.dart';
 
 import '../app_configs.dart';
-import '../models/enums/game_stage_enum.dart';
-import '../widgets/cards_view.dart';
+import '../providers/game_provider.dart';
+import '../widgets/time_display.dart';
 
 class OffPlay extends StatefulWidget {
   final int numberOfPlayers;
@@ -25,103 +26,75 @@ class OffPlay extends StatefulWidget {
 }
 
 class _OffPlayState extends State<OffPlay> {
-  late List<GameCard> gameCards;
-  late GameStageEnum currentGameStage;
-  late int secondsRemaining = 0;
-  late Timer? timer;
+  late GameProvider _gameProvider;
 
   @override
   void initState() {
     super.initState();
-    gameCards =
-        GameLogic.getItemsForCategory(widget.category, widget.numberOfPlayers);
-    currentGameStage = GameStageEnum.selectionStage;
+    _gameProvider = GameProvider();
+    _gameProvider.initializeGame(widget.numberOfPlayers, widget.category);
+    _gameProvider.roundLength = widget.roundLength;
   }
 
   @override
   void dispose() {
-    timer?.cancel();
+    _gameProvider.dispose();
     super.dispose();
   }
 
-  void startTimer(int seconds) {
-    secondsRemaining = seconds;
-    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
-      setState(() {
-        secondsRemaining--;
-        if (secondsRemaining == 0) {
-          t.cancel();
-          // Handle timer expiration logic here
-        }
-      });
-    });
-  }
-
-  void handleGameStageChange(GameStageEnum nextGameStage) {
+  void startRound() {
     setState(() {
-      currentGameStage = nextGameStage;
+      _gameProvider.gameStarted = true;
     });
-
-    if (currentGameStage == GameStageEnum.playStage) {
-      startTimer(AppConfigs.getRoundDuration(widget.roundLength));
-    }
-  }
-
-  void handleRoundEnd(bool isImposter) {
-    if (isImposter) {
-      //handle playersWon
-    } else {
-      //next round/ imposter won.
-    }
-  }
-
-  void handleGameCardVoting(int index) {
-    if (index < 0 || index > gameCards.length - 1) {
-      return;
-    }
-
-    bool isImposter = gameCards[index].isImposterCard;
-    handleRoundEnd(isImposter);
-
-    setState(() {
-      gameCards[index].isEliminated = true;
-    });
+    GameDialogs.showGameStartDialog(
+      context,
+      widget.category,
+      widget.numberOfPlayers,
+      widget.roundLength,
+      _gameProvider.startTimer,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppConfigs.getAppBar(statusBarHeight, true),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: AppConfigs.gameBackgroundGradient,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: statusBarHeight + 150,
-            ),
-            DisplayCards(
-              gameCards: gameCards,
-              currentGameStage: currentGameStage,
-              handleGameStageChange: handleGameStageChange,
-              handleGameCardVoting: handleGameCardVoting,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Time Remaining: $secondsRemaining seconds',
-                style: const TextStyle(fontSize: 18.0),
+
+    return ChangeNotifierProvider.value(
+      value: _gameProvider,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppConfigs.getAppBar(statusBarHeight, true),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: AppConfigs.gameBackgroundGradient,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: statusBarHeight + 150,
               ),
-            ),
-          ],
+              returnDisplayWidget(),
+              const TimeRemainingDisplay(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget returnDisplayWidget() {
+    if (_gameProvider.currentGameStage == GameStageEnum.selectionStage) {
+      return DisplaySelectionCards(
+        gameProvider: _gameProvider,
+        startRound: startRound,
+      );
+    } else if (_gameProvider.currentGameStage == GameStageEnum.playStage) {
+      return DisplayGameCards(gameProvider: _gameProvider);
+    }
+
+    return const Text("something went wrong");
   }
 }
